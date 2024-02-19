7ceb632b23a6f80273d9a66400310816030fdc2f
Chris Openshaw <copenshaw17@gmail.com>
2024-02-19 15:26:04 -0600

    a lot

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
-var mouseInEmbed = false
-
-document.getElementById("embed").addEventListener("mouseenter", function() {mouseInEmbed=true;console.log(mouseInEmbed)})
-document.getElementById("embed").addEventListener("mouseout", function() {mouseInEmbed=false;console.log(mouseInEmbed)})
-
 function change() {
     let range = document.getElementById("zoom-range")
     let rangeLabel = document.getElementById("range-label")
 
     rangeLabel.innerText = (range.value*10) + '%'
+}
+
+let pages = document.getElementsByClassName("main-page")
+for (page of pages) {
+    page.addEventListener("click", function() {
+    })
 }
