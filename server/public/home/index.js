3cd3115fe6005bfacbb338abbfcdb6b5d9ef5eb4
Chris Openshaw <copenshaw17@gmail.com>
2024-02-20 09:05:52 -0600

    More server stuff

---
+++ server/public/home/index.js
@@ -0,0 +1,53 @@
+function change() {
+    let range = document.getElementById("zoom-range")
+    let rangeLabel = document.getElementById("range-label")
+
+    rangeLabel.innerText = (range.value*10) + '%'
+}
+
+let pages = document.getElementsByClassName("main-page")
+for (page of pages) {
+    page.addEventListener("click", function() {
+    })
+}
+
+function getChildren(element) {
+    if (element.hasChildNodes()) {
+        console.log(element.children)
+    }
+}
+
+class EmbedData {
+    constructor(parentElement) {
+        this.parent = parentElement
+        this.embedJson = this.fillData(this.parent)
+    }
+
+    fillData(div) {
+        var tag = {}
+        tag['class']=div.className
+        tag['children'] = []
+        for(var i = 0; i< div.children.length;i++){
+            tag['children'].push(this.fillData(div.children[i]))
+        }
+        return tag
+    }
+}
+
+document.getElementById("save-btn").addEventListener("click", async function() {
+    let designBody = document.getElementById("designer-body")
+    let embedData = new EmbedData(designBody)
+    await fetch('/save', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(embedData.embedJson)
+    })
+       .then(response => response.json())
+       .then(response => console.log(JSON.stringify(response)))
+})     
+
+
+
--- server/server.go
+++ server/server.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("No env found")
+	}
+}
+
+func embedHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(b))
+}
+
 func main() {
-	fmt.Println("Running")
+	key, exists := os.LookupEnv("PORT")
+	if !exists {
+		log.Fatal("No port key found")
+	}
+	port := fmt.Sprintf(":%v", key)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./public/home"))))
+	http.HandleFunc("/save", embedHandler)
+
+	log.Println("Starting server")
+	err := http.ListenAndServe(port, nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
